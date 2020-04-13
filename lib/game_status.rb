89a9aa4fd483ecdcd6c4422933e35ef81aa2ea38
Amy Chin-Lai <amy.chinlai@gmail.com>
2020-04-13 20:49:43 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,26 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [0, 4, 8],
+  [0, 3, 6],
+  [3, 4, 5],
+  [6, 4, 2],
+  [1, 4, 7],
+  [6, 7, 8],
+  [2, 5, 8]
+]
+
+def won?(board)
+  if board == win_combinations.any?
+end
+end
+
+def full?(board)
+  (board).none?{|i| i == " "}
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+  end
