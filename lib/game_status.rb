64b38009ba551c04b4b9b66bc4229f216aee3731
Amy Chin-Lai <amy.chinlai@gmail.com>
2020-04-13 20:57:07 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,6 +16,7 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   winning_combination = WIN_COMBINATIONS.detect do |combination|
+    board[combination[0]] != " " && board[combination[0]] == board[combination[1]] && board[combination[1]] == board[combination[2]]
 end
 end
 
